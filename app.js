0eada46d658ffbfced229d1919caaf500e9c8453
Michael Benzinger <michaelbenzinger3@gmail.com>
2021-11-04 18:50:03 -0500

    initial commit after setting up dummy routes/controllers

---
+++ app.js
@@ -0,0 +1,57 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var mongoose = require('mongoose');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var catalogRouter = require('./routes/catalog');
+
+// Set up default mongoose connection
+var mongoDB = 'mongodb+srv://mbe-student:mbe-pug-devel@express-cluster.swpez.mongodb.net/express-cluster?retryWrites=true&w=majority';
+mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});
+
+// Get the default connection
+var db = mongoose.connection;
+
+// Bind connection to error event (to get notification of connection errors)
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+// Initialize Express App
+var app = express();
+
+// View Engine Setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+// Middleware Setup
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Router Setup
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/catalog', catalogRouter);
+
+// Catch 404 and forward to Error Handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// Error Handler
+app.use(function(err, req, res, next) {
+  // Set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // Render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
---
+++ models/author.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const AuthorSchema = new Schema({
+  first_name: {type: String, required: true, maxLength: 100},
+  last_name: {type: String, required: true, maxLength: 100},
+  date_of_birth: Date,
+  date_of_death: Date,
+});
+
+AuthorSchema.virtual('name').get(() => {
+  return `${this.last_name}, ${this.first_name}`;
+});
+
+AuthorSchema.virtual('lifespan').get(() => {
+  let lifetime_string = '';
+  if (this.date_of_birth) {
+    lifetime_string = DateTime.fromJSDate(this.date_of_birth).toLocaleString(DateTime.DATE_MED);
+  }
+  lifetime_string += ' - ';
+  if (this.date_of_death) {
+    lifetime_string = DateTime.fromJSDate(this.date_of_death).toLocaleString(DateTime.DATE_MED);
+  }
+  return lifetime_string;
+});
+
+AuthorSchema.virtual('url').get(() => {
+  return '/catalog/author/' + this._id;
+});
+
+module.exports = mongoose.model('Author', AuthorSchema);
