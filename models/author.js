d1c9b0c3e1c162b1eacf51bfb5336c0ce8393a20
Michael Benzinger <michaelbenzinger3@gmail.com>
2021-11-04 20:07:36 -0500

    Views and routes for all list and detail pages

--- models/author.js
+++ models/author.js
@@ -1,30 +1,33 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const { DateTime } = require('luxon');
 
 const AuthorSchema = new Schema({
   first_name: {type: String, required: true, maxLength: 100},
   last_name: {type: String, required: true, maxLength: 100},
-  date_of_birth: Date,
-  date_of_death: Date,
+  date_of_birth: {type: Date},
+  date_of_death: {type: Date},
 });
 
-AuthorSchema.virtual('name').get(() => {
+AuthorSchema.virtual('name').get(function() {
   return `${this.last_name}, ${this.first_name}`;
 });
 
-AuthorSchema.virtual('lifespan').get(() => {
+AuthorSchema.virtual('lifespan').get(function() {
   let lifetime_string = '';
   if (this.date_of_birth) {
     lifetime_string = DateTime.fromJSDate(this.date_of_birth).toLocaleString(DateTime.DATE_MED);
+  } else {
+    lifetime_string = '?'
   }
   lifetime_string += ' - ';
   if (this.date_of_death) {
-    lifetime_string = DateTime.fromJSDate(this.date_of_death).toLocaleString(DateTime.DATE_MED);
+    lifetime_string += DateTime.fromJSDate(this.date_of_death).toLocaleString(DateTime.DATE_MED);
   }
   return lifetime_string;
 });
 
-AuthorSchema.virtual('url').get(() => {
+AuthorSchema.virtual('url').get(function() {
   return '/catalog/author/' + this._id;
 });
 
--- models/bookinstance.js
+++ models/bookinstance.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const { DateTime } = require('luxon');
 
 const BookInstanceSchema = new Schema({
   book: { type: Schema.Types.ObjectId, ref: 'Book', required: true },
@@ -8,8 +9,12 @@ const BookInstanceSchema = new Schema({
   due_back: {type: Date, default: Date.now}
 });
 
-BookInstanceSchema.virtual('url').get(() => {
+BookInstanceSchema.virtual('url').get(function() {
   return '/catalog/bookinstance/' + this._id;
+});
+
+BookInstanceSchema.virtual('due_back_formatted').get(function() {
+  return DateTime.fromJSDate(this.due_back).toLocaleString(DateTime.DATE_MED);
 });
 
 module.exports = mongoose.model('BookInstance', BookInstanceSchema);
